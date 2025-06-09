8aec41b0cd3adb22cf8be38a13ed3d4abda4b26f
Jairo Beltrán <jbeltranjimenez@gmail.com>
2025-06-08 19:13:00 -0500

    fotos en boton

--- css/conocenos.js
+++ css/conocenos.js
@@ -1,43 +1,12 @@
-var full=false;
+document.getElementById("botonFotos").addEventListener("click", function() {
+    const galeria = document.getElementById("galeria");
+    
+    if (galeria.style.display === "none") {
+        galeria.style.display = "flex"; // Muestra la galería
+        this.textContent = "Ocultar Fotos"; // Cambia el texto del botón
+    } else {
+        galeria.style.display = "none";
+        this.textContent = "Ver Fotos";
+    }
+    });
 
-$('.imgWrap').hover(function () {
- // $(this).css('width', '36%');
-   $(this).addClass('current');
-  //$(this).siblings().css('width', '16%');  
-   $(this).siblings().addClass('notCurrent');
-}, function () {
-  //$('.imgWrap').css('width', '20%');  
-  $('.imgWrap').siblings().removeClass('notCurrent');
-  $('.imgWrap').removeClass('current');
- });
-
-$('.imgWrap').click(function () {  
-  
-  if(!full){
-    full = true;
-    // $(this).css('width', '100%');
-    $(this).addClass('current-full');
-  //$(this).siblings().css('width', '0%');  
-   $(this).siblings().addClass('notCurrent-full');
-  }
-});
-
-$('.mfp-close').click(function () {
-  full = false;
-  $('.imgWrap').siblings().removeClass('notCurrent-full');
-  $('.imgWrap').removeClass('current-full');
- 
-});
-
-
-$('.gallery').magnificPopup({
-  delegate: 'a',
-  type: 'image',
-  removalDelay: 300,
-  mainClass: 'mfp-fade',
-  gallery: {
-    enabled: true,
-    navigateByImgClick: true,
-    preload: [0,1]
-  }
-});
